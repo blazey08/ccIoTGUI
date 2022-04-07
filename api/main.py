718da8a17378245449b0fbdeb2fca79f23c30a59
Avery Lau <yuhui_lau@mymail.sutd.edu.sg>
2022-04-07 23:29:16 +0800

    Have to try to get both image and tags in the api folder

--- api/main.py
+++ api/main.py
@@ -1,12 +1,36 @@
-import time 
-from flask import Flask
+from asyncore import file_wrapper
+from fileinput import filename
+import os
+from flask_cors import CORS
+import re
+from flask import Flask, request, jsonify
+import boto3
+import json
 
 app = Flask(__name__)
+CORS(app)
+UPLOAD_FOLDER = os.path.join(os.path.abspath("."),"images")
 
-@app.route('/time')
+@app.route('/')
 def get_current_time():
-    return {'time': time.time(), 'string':"This is the data"}
+    return {"title": "CCIOT Application", "home": "here"}
 
-@app.route('/home')
-def home_page():
-    return {"here": "yuyan"}
+# Handles file upload
+@app.route('/upload', methods = ["POST"], strict_slashes = False)
+def upload_page():
+    # img = request.files['file']
+    # tags = img.headers
+    # print(tags)
+    # img.save(os.path.join(UPLOAD_FOLDER, img.filename))    
+
+    data = request.form['tag']
+
+    print(data)
+
+    # s3 = boto3.client("s3")
+    # metadata = "topics=['Fire']"
+    # s3.upload_file("images/fire.jpg","cciot-fastgame-proj-ads","fire.jpg")
+
+    # response = jsonify({'some': 'data'})
+    # response.headers.add('Access-Control-Allow-Origin', '*')
+    return {"Ack":"Message"}
